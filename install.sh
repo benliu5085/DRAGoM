388a3f6c939f60f6d4ec24d26ff86529ae54d076
benliu5085 <47131361+benliu5085@users.noreply.github.com>
2020-10-04 10:43:15 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@
 #------------------------
 # Pre-requisites needed
 #------------------------
-# cmake :
-# zlib :
-# gcc version:
+# cmake : 3.12
+# make : 3.82
+# gcc version: 4.8.5
 #
 #
 #
@@ -15,37 +15,37 @@ export DROGOM_HOME=`pwd`
 rm -rf "env.config"
 echo "home " $DROGOM_HOME >> env.config
 
-echo -e "\nInstalling SGA..."
+echo -e "Installing SGA..."
 cd $DROGOM_HOME/lib
 tar xvzf sga.tar.gz
 echo "sga " $DROGOM_HOME"/lib/sga" >> $DROGOM_HOME/env.config
 
-echo -e "\n\nInstalling SPAdes..."
+echo -e "\nInstalling SPAdes..."
 cd $DROGOM_HOME/lib
 tar xvzf SPAdes.tar.gz
 echo "spades " $DROGOM_HOME"/lib/SPAdes/bin/spades.py"  >> $DROGOM_HOME/env.config
 
-echo -e "\n\nInstalling BWA..."
+echo -e "\nInstalling BWA..."
 cd $DROGOM_HOME/lib
 tar xvzf bwa.tar.gz
 echo "bwa " $DROGOM_HOME"/lib/bwa"  >> $DROGOM_HOME/env.config
 
-echo -e "\n\nInstalling CDHIT..."
+echo -e "\nInstalling CDHIT..."
 cd $DROGOM_HOME/lib
 tar xvzf cd-hit-est.tar.gz
 echo "cdhit " $DROGOM_HOME"/lib/cd-hit-est"  >> $DROGOM_HOME/env.config
 
-echo -e "\n\nInstalling CMPress..."
+echo -e "\nInstalling CMPress..."
 cd $DROGOM_HOME/lib
 tar xvzf cmpress.tar.gz
 echo "cmpress " $DROGOM_HOME"/lib/cmpress"  >> $DROGOM_HOME/env.config
 
-echo -e "\n\nInstalling CMSearch..."
+echo -e "\nInstalling CMSearch..."
 cd $DROGOM_HOME/lib
 tar xvzf cmsearch.tar.gz
 echo "cmsearch " $DROGOM_HOME"/lib/cmsearch"  >> $DROGOM_HOME/env.config
 
-echo -e "\n\nInstalling Samtools..."
+echo -e "\nInstalling Samtools..."
 cd $DROGOM_HOME/lib
 if test `cat /etc/os-release | grep -c 'Ubuntu'` -gt 0 ; then
   tar xvzf samtools_ubuntu.gz.tar
@@ -55,18 +55,25 @@ fi
 echo "samtools " $DROGOM_HOME"/lib/samtools"  >> $DROGOM_HOME/env.config
 
 echo -e "\nCompleted installing all third-party tools."
-echo -e "\n----------------------------------------------\n"
+echo -e "----------------------------------------------"
 ## DRAGoM installation
-echo -e "\nInstalling DRAGoM ..."
-if [ ! -e $DROGOM_HOME/bin ]; then
-        mkdir $DROGOM_HOME/bin
-fi
+echo -e "Installing DRAGoM ..."
+mkdir -p $DROGOM_HOME/bin
 cd $DROGOM_HOME/src
-make clean
-make
-echo "dragom " $DROGOM_HOME"/bin/dragom.exe"  >> $DROGOM_HOME/env.config
+cmake .
 
-cd $DROGOM_HOME
-echo -e "\n-----------------------------------------------\n"
-echo -e "\nSuccessfully installed DRAGoM.\n"
-echo -e "\n-----------------------------------------------\n"
+if test -f "Makefile"; then
+	make
+	mv dragom.exe ../bin
+	echo "dragom " $DROGOM_HOME"/bin/dragom.exe"  >> $DROGOM_HOME/env.config
+
+	cd $DROGOM_HOME
+	echo -e "\n-----------------------------------------------\n"
+	echo -e "Successfully installed DRAGoM."
+	echo -e "\n-----------------------------------------------\n"
+else
+	echo -e "\n-----------------------------------------------\n"
+	echo -e "Failed to installed DRAGoM."
+	echo -e "Check the error massage above"
+	echo -e "\n-----------------------------------------------\n"
+fi
